1aa9a8cbbe64b60ab8e0451fed4044b0f3cbd5ad
David Marshall <marshall.david78@gmail.com>
2022-07-29 23:02:49 +0000

    -

---
+++ projects/lib-rect/src/config.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core'
+import * as fs from 'fs'
+import * as json5 from 'json5'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RgConfig {
+  fileName = 'config.json'
+  config: any
+
+  constructor() {
+    const jsonData = fs.readFileSync(this.fileName, { encoding: 'utf-8' })
+    this.config = json5.parse(jsonData)
+  }
+}
